a55802890e1b2132ee92aceb7ffbce0f1794ec9a
aslak <s331378@oslomet.no>
2020-08-25 14:54:26 +0200

    Oppretter repository

---
+++ app/src/main/java/com/example/s331378_s331389_mappe1baresum/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.s331378_s331389_mappe1baresum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
