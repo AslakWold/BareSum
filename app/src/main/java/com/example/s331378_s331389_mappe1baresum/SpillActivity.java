d4d5c2183a50b9aa19bfd4e64b0faf06d69b1a4f
Jacob Tærud <jacob99@live.no>
2020-09-10 16:23:24 +0200

    Lagt til en metode for å legge inn verdi når man trykker på et tall Metode for å fjerne det siste tallet man skrev inn
    Ingen metoder testet enda

--- app/src/main/java/com/example/s331378_s331389_mappe1baresum/SpillActivity.java
+++ app/src/main/java/com/example/s331378_s331389_mappe1baresum/SpillActivity.java
@@ -1,13 +1,17 @@
 package com.example.s331378_s331389_mappe1baresum;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SpillActivity extends AppCompatActivity {
 
     String [] oppgaverOgSvar;
+    int antRiktige;
 
 
     protected void onCreate(Bundle savedInstanceState){
@@ -17,5 +21,40 @@ public class SpillActivity extends AppCompatActivity {
         Resources res = getResources();
         oppgaverOgSvar = res.getStringArray(R.array.matteoppgaver);
     }
+
+    public void leggInnVerdi(TextView tv, int verdi){
+        String gmlVerdi = (String)tv.getText();
+        if(gmlVerdi!=null) {
+            tv.setText(gmlVerdi + verdi);
+        }else{
+            tv.setText(verdi);
+        }
+    }
+
+
+    //metode som fjerner siste tall i svarboksen
+
+    /*
+    //Må legge inn textview og knapp i "guiet".
+    public void btnfjern(View v){
+
+        TextView svar = (TextView)findViewById(R.id.txtSvar);
+
+        String gmlSvar = txtSvar.getText();
+        char [] nyttSvarArray = gmlSvar.toCharArray();
+        String nyttSvar = "";
+        for(int i = 0; i < nyttSvarArray.length - 1; i++){
+            nyttSvar+=nyttSvarArray[i];
+        }
+
+        txtSvar.setText(nyttSvar);
+
+
+    }*/
+
+
+    /*public void btnLeverOppgave(View v){
+
+    }*/
 
 }
