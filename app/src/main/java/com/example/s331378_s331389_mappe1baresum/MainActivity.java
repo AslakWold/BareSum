43beac34ba279807142afc1a4a00c3ea1973d862
aslak <s331378@oslomet.no>
2020-09-14 12:53:59 +0200

    + settland(String landskode) {} + tysk(View v) + norsk(View v) + De tre metodene over skal gjøre at vi kan endre språk i preferansevinduet.

--- app/src/main/java/com/example/s331378_s331389_mappe1baresum/MainActivity.java
+++ app/src/main/java/com/example/s331378_s331389_mappe1baresum/MainActivity.java
@@ -3,9 +3,14 @@ package com.example.s331378_s331389_mappe1baresum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,4 +33,24 @@ public class MainActivity extends AppCompatActivity {
     public void btnSeStatistikk(View v) {
 
     }
+
+    //kode for endring av språk
+
+    public void settland(String landskode) {
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration cf = res.getConfiguration();
+        cf.setLocale(new Locale(landskode));
+        res.updateConfiguration(cf, dm);
+    }
+
+    public void tysk(View v) {
+        settland("de");
+        recreate();
+    }
+
+    public void norsk(View v) {
+        settland("no");
+        recreate();
+    } //endring av språk
 }
